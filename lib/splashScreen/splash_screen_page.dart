a4a8069f1eb3de2c829b3330f00571c87a5e5636
ajitesh gowlikar <ajiteshgowlikar@gmail.com>
2025-03-02 20:44:00 +0530

    animation added to splash screen

--- lib/splashScreen/splash_screen_page.dart
+++ lib/splashScreen/splash_screen_page.dart
@@ -9,11 +9,24 @@ class SplashScreenPage extends StatefulWidget {
   State<SplashScreenPage> createState() => _SplashScreenPageState();
 }
 
-class _SplashScreenPageState extends State<SplashScreenPage> {
+class _SplashScreenPageState extends State<SplashScreenPage>
+    with SingleTickerProviderStateMixin {
+  late Animation colorAnimation;
+  late AnimationController colorAnimationController;
   @override
   void initState() {
     super.initState();
-    Timer(const Duration(seconds: 60), () {
+    colorAnimationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 10),
+      animationBehavior: AnimationBehavior.preserve,
+    );
+    colorAnimation = ColorTween(
+      begin: Colors.green[100],
+      end: Colors.green,
+    ).animate(colorAnimationController);
+    colorAnimationController.forward();
+    Timer(const Duration(seconds: 15), () {
       Navigator.pushReplacement(
         context,
         MaterialPageRoute(builder: (context) => const NewLayout()),
@@ -26,13 +39,22 @@ class _SplashScreenPageState extends State<SplashScreenPage> {
     return Scaffold(
       body: Center(
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
           mainAxisAlignment: MainAxisAlignment.center,
-          children: const [
+          children: [
             // FlutterLogo(size: 100),
-            Icon(Icons.eco, size: 150),
-            SizedBox(height: 20),
+            AnimatedBuilder(
+              animation: colorAnimation,
+              builder:
+                  (context, child) =>
+                      Icon(Icons.eco, size: 150, color: colorAnimation.value),
+            ),
+            const SizedBox(height: 20),
             // CircularProgressIndicator(),
-            Text('Clean Green Future', style: TextStyle(fontSize: 20)),
+            const Text(
+              'Clean Green Future',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
           ],
         ),
       ),
